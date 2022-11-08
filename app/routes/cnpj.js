df77a31a1cff76a1e4116746c93e44ce2822e601
Nicholas Galipi Gomes <nicholas.gomes@nfe.io>
2022-11-08 20:06:59 -0300

    test retrivieng json file

--- app/routes/cnpj.js
+++ app/routes/cnpj.js
@@ -4,7 +4,7 @@ export default class CnpjRoute extends Route {
   async model(params) {
     let { cnpj } = params;
 
-    let response = await fetch(`./../../${cnpj}.json`);
+    let response = await fetch(`https://consulta-5b1ff.firebaseapp.com/${cnpj}.json`);
     let { legalEntity } = await response.json();
 
     console.log(legalEntity);
