996453f7a3cd4fd5ae8c214fa8f438f41a9b3ca1
Nicholas Galipi Gomes <nicholas.gomes@nfe.io>
2022-11-08 19:48:09 -0300

    changes on cnpj router

--- app/routes/cnpj.js
+++ app/routes/cnpj.js
@@ -4,7 +4,7 @@ export default class CnpjRoute extends Route {
   async model(params) {
     let { cnpj } = params;
 
-    let response = await fetch('./../../google.json');
+    let response = await fetch(`./../../${cnpj}.json`);
     let { legalEntity } = await response.json();
 
     console.log(legalEntity);
